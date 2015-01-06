b43f974347b58ba71e86a4db21a8be521ae35be1
James Adams <james@makenub.com>
2015-01-06 14:00:13 -0700

    Faster digitalWrite

--- SparkChristmasLights.ino
+++ SparkChristmasLights.ino
@@ -676,13 +676,14 @@ void handleDimmingTimerInterrupt()
     byte *tempPWMValues = &pwmValues[numberOfChannels];
     
     // Write shift register latch clock low
-    digitalWrite(latchPin, LOW);
+    //digitalWrite(latchPin, LOW);
+    PIN_MAP[latchPin].gpio_peripheral->BRR = PIN_MAP[latchPin].gpio_pin;  // LOW
     
     // Do a whole shift register at once. This unrolls the loop for extra speed
     for(byte i = numberOfShiftRegisters; i > 0; --i)
     {
         /*byte sendbyte;  // no need to initialize, all bits are replaced
-        
+         
          // Build the byte. One bit for each channel in this shift register
          add_one_pin_to_byte(sendbyte, currentBrightnessCounter, --tempPWMValues);
          add_one_pin_to_byte(sendbyte, currentBrightnessCounter, --tempPWMValues);
@@ -705,11 +706,12 @@ void handleDimmingTimerInterrupt()
         }
         
         // Send the byte to the SPI
-        SPI.transfer(sendbyte);
+        SPI.transfer(byteToSend);
     }
     
     // Write shift register latch clock high
-    digitalWrite(latchPin, HIGH);
+    //digitalWrite(latchPin, HIGH);
+    PIN_MAP[latchPin].gpio_peripheral->BSRR = PIN_MAP[latchPin].gpio_pin; // HIGH
     
     // Decrease the brightness index. This is the key to getting AC dimming since triacs stay on until the next zero cross. So we need to turn on after a delay rather than turn on immediately and turn off after a delay. AKA, this needs to count down not up. Trust me!
     if(currentBrightnessCounter > 0)
@@ -779,3 +781,4 @@ void dimmingTest()
 }
 
 #endif
+
